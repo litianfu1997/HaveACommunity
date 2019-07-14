31d23224626f9aad78b7e17e07ed89c0c0ee4857
litianfu <1035869369@qq.com>
2019-07-14 17:43:42 +0800

    "flyway数据库插件的使用"

---
+++ src/main/java/com/tech4flag/community/mapper/QuestionMapper.java
@@ -0,0 +1,32 @@
+package com.tech4flag.community.mapper;
+
+import com.tech4flag.community.model.Question;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @author litianfu
+ * @version 1.0
+ * @email 1035869369@qq.com
+ * @date 2019-07-13 17:38
+ * create table question
+ * (
+ * id int auto_increment,
+ * title varchar(50),
+ * gmt_create bigint,
+ * gmt_modified bigint,
+ * creator int,
+ * comment_count int default 0,
+ * view_count int default 0,
+ * like_count int default 0,
+ * tag varchar(256),
+ * constraint question_pk
+ * primary key (id)
+ * );
+ */
+@Mapper
+public interface QuestionMapper {
+    @Insert("insert into question(title,description,gmt_create,gmt_modified,creator,tag)" +
+            "values(#{title},#{description},#{gmtCreate},#{gmtModified},#{creator},#{tag})")
+    void create(Question question);
+}
---
+++ src/main/java/com/tech4flag/community/model/Question.java
@@ -0,0 +1,95 @@
+package com.tech4flag.community.model;
+
+import lombok.Data;
+
+/**
+ * @author litianfu
+ * @version 1.0
+ * @email 1035869369@qq.com
+ * @date 2019-07-14 00:03
+ * values(#{title},#{description},#{gmtCreate},#{gmtModified},#{creator},#{tag})
+ */
+
+public class Question {
+    private Integer id;
+    private String title;
+    private String description;
+    private String tag;
+    private Long gmtCreate;
+    private Long gmtModified;
+    private Integer creator;
+    private Integer viewCount;
+    private Integer likeCount;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public Long getGmtCreate() {
+        return gmtCreate;
+    }
+
+    public void setGmtCreate(Long gmtCreate) {
+        this.gmtCreate = gmtCreate;
+    }
+
+    public Long getGmtModified() {
+        return gmtModified;
+    }
+
+    public void setGmtModified(Long gmtModified) {
+        this.gmtModified = gmtModified;
+    }
+
+    public Integer getCreator() {
+        return creator;
+    }
+
+    public void setCreator(Integer creator) {
+        this.creator = creator;
+    }
+
+    public Integer getViewCount() {
+        return viewCount;
+    }
+
+    public void setViewCount(Integer viewCount) {
+        this.viewCount = viewCount;
+    }
+
+    public Integer getLikeCount() {
+        return likeCount;
+    }
+
+    public void setLikeCount(Integer likeCount) {
+        this.likeCount = likeCount;
+    }
+}
